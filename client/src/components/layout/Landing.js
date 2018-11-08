b7e06bf528f9a8324fd807fdb03187a7334ce4dc
Tristan Anderl <tanderl@apartments247.com>
2018-11-08 12:28:06 -0800

    design changes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,9 +17,9 @@ class Landing extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12 text-center">
-                            <h1 className="display-3 mb-4">Developer Connector
+                            <h1 className="display-3 mb-4">connectUNLV
                             </h1>
-                            <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+                            <p className="lead"> Create profile, share posts and get help from other computer science students</p>
                             <hr />
                             <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
                             <Link to="/login" className="btn btn-lg btn-light">Login</Link>
